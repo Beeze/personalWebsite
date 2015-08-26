b840f1f922152d947f58b6a11ee4d905e071ba25
Alona King <alonaking13@gmail.com>
2015-08-26 02:24:01 -0500

    need to put in links to github source code and adding more projects

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -1,4 +1,12 @@
    $('a[href*=#]:not([href=#])').click(function() {
+       if( $(this).attr("href")=="#myCarousel1") return;
+       if( $(this).attr("href")=="#myCarousel2") return;
+       if( $(this).attr("href")=="#myCarousel3") return;
+       if( $(this).attr("href")=="#myCarousel4") return;
+       if( $(this).attr("href")=="#myCarousel5") return;
+       if( $(this).attr("href")=="#myCarousel6") return;
+       if( $(this).attr("href")=="#myCarousel7") return;
+       if( $(this).attr("href")=="#myCarousel8") return;
            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
                        || location.hostname == this.hostname) {
 
